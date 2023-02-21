27edb87d5babb46078c7fd1d43a23a1965a98ce6
TomTheCodingGuy <124187473+TomTheCodingGuy@users.noreply.github.com>
2023-02-21 18:14:54 +0000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,46 @@
+#!/bin/bash
+version=0.0.1
+
+echo "Creating Directories..."
+rm -rf /tmp/Cow-Bounce
+mkdir -p /tmp/ || error "Could not make directory!"
+cd /tmp/
+echo "Complete!"
+
+# Downloading Git-Hub Repository
+echo "Downloading Repository..."
+git_clone https://github.com/TomTheCodingGuy/Cow-Bounce.git || error 'Failed to clone repository!'
+echo "Complete!"
+
+# Move files to /opt
+echo "Moving Files..."
+sudo mv -f /tmp/Cow-Bounce/ /opt/Cow-Bounce/ || error "Failed to move source folder to /opt!"
+echo "Complete!"
+
+# Install some packages that are necessary to run this app - no need for "error", as the install_packages function already handles errors.
+install_packages python3 || exit 1
+
+# Installing PyGame Python Library
+echo "Installing PyGame..."
+pip install pygame || error "Failed to install PyGame"
+echo "Complete!"
+
+#Create cowbounce command
+sudo mkdir -p /usr/local/bin
+echo '#!/bin/bash
+cd /opt/Cow-Bounce/
+sudo chmod +x run.sh
+./run.sh' | sudo tee /usr/local/bin/cowbounce >/dev/null
+sudo chmod +x /usr/local/bin/cowbounce
+
+#Menu shortcut
+sudo mkdir -p /usr/local/share/applications
+echo "[Desktop Entry]
+Name=CowBounce!
+Comment=Fun Vertical PLatformer!
+Icon=$(dirname "$0")/icon-64.png
+Exec=cowbounce
+Path=/opt/Cow-Bounce/
+Type=Application
+Terminal=false
+Categories=Game;" | sudo tee /usr/local/share/applications/CowBounce!.desktop >/dev/null
