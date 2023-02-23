01a7f4ecaee2bfb272444d539a83f273d5343479
TomTheCodingGuy <124187473+TomTheCodingGuy@users.noreply.github.com>
2023-02-23 17:13:07 +0000

    Update uninstall.sh

--- uninstall.sh
+++ uninstall.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-sudo rm -rf /opt/Cow-Bounce/ /usr/local/share/applications/CowBounce!.desktop ~/.local/share/applications/CowBounce!.desktop /usr/local/bin/cowbounce || error "Could not remove CowBounce files!"
+sudo rm -rf /opt/Cow-Bounce/ /usr/local/share/applications/CowBounce!.desktop ~/.local/share/applications/CowBounce!.desktop /usr/local/bin/cowbounce 
