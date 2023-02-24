90c694f6ea2a6951f2c20ff301edc404ef161718
TomTheCodingGuy <124187473+TomTheCodingGuy@users.noreply.github.com>
2023-02-24 17:18:59 +0000

    Delete uninstall.sh

--- uninstall.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-echo "Removing Files..."
-sudo rm -rf /opt/Cow-Bounce/ /usr/local/share/applications/CowBounce!.desktop ~/.local/share/applications/CowBounce!.desktop /usr/local/bin/cowbounce 
-echo "Complete!"
